a5df1f5f9349a90d746db4356bb2699900fd84ea
olga_p <olgapsareva@yahoo.com>
2021-07-08 02:58:34 +0300

    MVVM architecture, viewBinding, project reorganisation

---
+++ app/src/main/java/ru/geekbrains/myweatherapp/model/RepositoryImpl.kt
@@ -0,0 +1,12 @@
+package ru.geekbrains.myweatherapp.model
+
+class RepositoryImpl : Repository {
+
+    override fun getWeatherFromServer(): Weather {
+        return Weather()
+    }
+
+    override fun getWeatherFromLocalStorage(): Weather {
+        return Weather()
+    }
+}
